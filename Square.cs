6c743a948d738280494dcc8c9190b3207a35c033
EesaNHS <eesa.mahmood1@nhs.net>
2023-05-04 13:12:33 +0100

    Add Square class and update csproj file.

---
+++ Square.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalProgammingLanguage
+{
+    internal class Square : Rectangle
+    {
+        readonly int length;
+        public Square(int x, int y, int length) : base(x, y, length, length)
+        {
+            this.length = length;
+        }
+
+
+        public override void Draw(Graphics g, Pen pen, Brush brush)
+        {
+            base.Draw(g, pen, brush);
+        }
+    }
+}
