af8bbed0faf2f995cd63369e3701633b26695d9e
EesaNHS <eesa.mahmood1@nhs.net>
2023-05-04 14:28:53 +0100

    Add button actions and console log commands to verify buttons work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,98 +30,109 @@
         {
             this.CommandPanel = new System.Windows.Forms.TextBox();
             this.Run = new System.Windows.Forms.Button();
-            this.Clear = new System.Windows.Forms.Button();
             this.CommandLine = new System.Windows.Forms.TextBox();
-            this.Save = new System.Windows.Forms.Button();
             this.Load = new System.Windows.Forms.Button();
-            this.Syntax = new System.Windows.Forms.Button();
             this.display = new System.Windows.Forms.PictureBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Syntax = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.display)).BeginInit();
             this.SuspendLayout();
             // 
             // CommandPanel
             // 
-            this.CommandPanel.Location = new System.Drawing.Point(30, 12);
+            this.CommandPanel.Location = new System.Drawing.Point(40, 15);
+            this.CommandPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CommandPanel.Multiline = true;
             this.CommandPanel.Name = "CommandPanel";
-            this.CommandPanel.Size = new System.Drawing.Size(368, 287);
+            this.CommandPanel.Size = new System.Drawing.Size(489, 352);
             this.CommandPanel.TabIndex = 0;
             // 
             // Run
             // 
-            this.Run.Location = new System.Drawing.Point(30, 349);
+            this.Run.Location = new System.Drawing.Point(40, 430);
+            this.Run.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Run.Name = "Run";
-            this.Run.Size = new System.Drawing.Size(75, 23);
+            this.Run.Size = new System.Drawing.Size(100, 28);
             this.Run.TabIndex = 1;
             this.Run.Text = "Run";
             this.Run.UseVisualStyleBackColor = true;
-            this.Run.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Clear
-            // 
-            this.Clear.Location = new System.Drawing.Point(111, 349);
-            this.Clear.Name = "Clear";
-            this.Clear.Size = new System.Drawing.Size(75, 23);
-            this.Clear.TabIndex = 2;
-            this.Clear.Text = "Clear";
-            this.Clear.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.runButton);
             // 
             // CommandLine
             // 
-            this.CommandLine.Location = new System.Drawing.Point(30, 313);
+            this.CommandLine.Location = new System.Drawing.Point(40, 385);
+            this.CommandLine.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CommandLine.Name = "CommandLine";
-            this.CommandLine.Size = new System.Drawing.Size(368, 20);
+            this.CommandLine.Size = new System.Drawing.Size(489, 22);
             this.CommandLine.TabIndex = 3;
-            // 
-            // Save
-            // 
-            this.Save.Location = new System.Drawing.Point(607, 349);
-            this.Save.Name = "Save";
-            this.Save.Size = new System.Drawing.Size(75, 23);
-            this.Save.TabIndex = 4;
-            this.Save.Text = "Save";
-            this.Save.UseVisualStyleBackColor = true;
             // 
             // Load
             // 
-            this.Load.Location = new System.Drawing.Point(688, 349);
+            this.Load.Location = new System.Drawing.Point(917, 430);
+            this.Load.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Load.Name = "Load";
-            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.Size = new System.Drawing.Size(100, 28);
             this.Load.TabIndex = 5;
             this.Load.Text = "Load";
             this.Load.UseVisualStyleBackColor = true;
-            this.Load.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // Syntax
-            // 
-            this.Syntax.Location = new System.Drawing.Point(323, 349);
-            this.Syntax.Name = "Syntax";
-            this.Syntax.Size = new System.Drawing.Size(75, 23);
-            this.Syntax.TabIndex = 6;
-            this.Syntax.Text = "Syntax";
-            this.Syntax.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.loadButton);
             // 
             // display
             // 
-            this.display.Location = new System.Drawing.Point(444, 12);
+            this.display.Location = new System.Drawing.Point(592, 15);
+            this.display.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.display.Name = "display";
-            this.display.Size = new System.Drawing.Size(332, 287);
+            this.display.Size = new System.Drawing.Size(443, 353);
             this.display.TabIndex = 7;
             this.display.TabStop = false;
             // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(147, 430);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(104, 28);
+            this.Clear.TabIndex = 8;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.clearButton);
+            // 
+            // Syntax
+            // 
+            this.Syntax.Location = new System.Drawing.Point(415, 430);
+            this.Syntax.Name = "Syntax";
+            this.Syntax.Size = new System.Drawing.Size(103, 28);
+            this.Syntax.TabIndex = 9;
+            this.Syntax.Text = "Syntax";
+            this.Syntax.UseVisualStyleBackColor = true;
+            this.Syntax.Click += new System.EventHandler(this.syntaxButton);
+
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(802, 430);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(97, 28);
+            this.Save.TabIndex = 10;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.saveButton);
+
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.display);
-            this.Controls.Add(this.Syntax);
-            this.Controls.Add(this.Load);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.Save);
-            this.Controls.Add(this.CommandLine);
+            this.Controls.Add(this.Syntax);
             this.Controls.Add(this.Clear);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.CommandLine);
             this.Controls.Add(this.Run);
             this.Controls.Add(this.CommandPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.display)).EndInit();
@@ -134,12 +145,12 @@
 
         private System.Windows.Forms.TextBox CommandPanel;
         private System.Windows.Forms.Button Run;
-        private System.Windows.Forms.Button Clear;
         private System.Windows.Forms.TextBox CommandLine;
-        private System.Windows.Forms.Button Save;
         private System.Windows.Forms.Button Load;
-        private System.Windows.Forms.Button Syntax;
         private System.Windows.Forms.PictureBox display;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Syntax;
+        private System.Windows.Forms.Button Save;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,21 +10,47 @@ using System.Windows.Forms;
 
 namespace GraphicalProgammingLanguage
 {
+    /// <summary>
+    /// Form Class used to control application actions and events.
+    /// </summary>
     public partial class Form1 : Form
     {
+
+        private int x, y = 0;//default draw position
+        private int errorcount = 0;//used to determine number of errors generated
+
+        private Color pencol = Color.Black;//universal colour for pen
+        private Color brushcol = Color.Cyan;// universal colour for brush
+        private Random rnd = new Random();//used to make random number for colour changes and factory shapes
+
+        private Shape shape1; // used in producing shapes
+        private ShapeTypes factory = new ShapeTypes();// used to determine shape type of command
         public Form1()
         {
             InitializeComponent();
+            this.display.Image = new Bitmap(Size.Width, Size.Height);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void runButton(object sender, EventArgs e)
         {
-
+            Console.WriteLine("Run");
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void loadButton(object sender, EventArgs e)
         {
-
+            Console.WriteLine("Load");
+        }
+        private void clearButton(object sender, EventArgs e)
+        {
+            Console.WriteLine("Clear");
+        }
+        private void syntaxButton(object sender, EventArgs e)
+        {
+            Console.WriteLine("Syntax");
+        }
+        private void saveButton(object sender, EventArgs e)
+        {
+            Console.WriteLine("Save");
         }
     }
 }
